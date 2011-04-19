be35fa8569dac9642b82642f764044f2ca940541
Blake <blake.shiver@gmail.com>
2011-04-19 18:23:25 -0400

    Added validations to estimates model

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,2 +1,29 @@
 class Estimate < ActiveRecord::Base
+  
+  before_validation(:on => :create) do
+    self.home_phone = home_phone.gsub(/[^0-9]/, "")
+  end
+
+
+  validates :name,
+      :presence => true
+
+  validates :email,
+      :presence => true
+
+  validates :address1,
+      :presence => true
+
+  validates :city,
+      :presence => true
+
+  validates :state,
+      :presence => true
+
+  validates :zip,
+      :presence => true
+
+  validates :description,
+      :presence => true
+  
 end
